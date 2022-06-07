62a25dcca1d07a4a7b13bb8e799ff2deda71f2e6
Kyle B. Johnson <kyle.johnson@hey.com>
2022-06-07 12:16:44 -0400

    First pass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import {useState} from "react";
+
+import useKeypress from 'react-use-keypress';
+import Confetti from 'react-confetti'
+import { useWindowSize } from '@react-hook/window-size'
+
+import UIfx from 'uifx'
+const beep = new UIfx(require('./audio/beep.mp3'))
+const confirm = new UIfx(require('./audio/confirm.wav'))
+const complete = new UIfx(require('./audio/complete.wav'))
+
 import './App.css';
 
 function App() {
+
+    const formatTargetName = ( text ) => text.toLowerCase().split('')
+
+    const [ targetName, setTargetName ] = useState( formatTargetName('planck') );
+    const [ progress, setProgress ] = useState('');
+
+    const nextLetter = [...targetName].splice( 0, progress.length + 1 ).pop();
+    console.log( 'NEXT: ' + nextLetter )
+
+    // if( targetName.length === progress.length ) {
+    //     complete.play();
+    // }
+
+    useKeypress('abcdefghijklmnopqrstuvwxyz'.split(''), (event) => {
+        console.log( 'Keypress: ' + event.key)
+        if ( event.key === nextLetter && targetName.length !== progress.length ) {
+            setProgress(progress + nextLetter)
+            confirm.play()
+        } else {
+            beep.play()
+        }
+    })
+
+    const [width, height] = useWindowSize()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div style={{height:'100vh',display:'flex',justifyContent:'center',alignItems:'center',gap:'20px'}}>
+            {targetName.map(( letter, index ) => {
+                const directory = letter === progress[ index ] ? 'lineal-color' : 'lineal';
+                return <img key={index} className={letter} style={{width:'100px'}} src={require('./images/alphabet/' + directory + '/' + letter + '.png')}/>
+            })}
+        </div>
+          { targetName.length === progress.length && (<Confetti
+              width={width}
+              height={height}
+          />)}
+          <div style={{position:'fixed', right:'20px', bottom:'20px'}}>
+                <button style={{border:0,backgroundColor:"transparent"}} onClick={() => {
+                    setTargetName( formatTargetName( window.prompt('Enter a word')))
+                }}>
+                    <img style={{width: '50px'}} src={require('./images/alphabet/lineal/alphabet.png')} />
+                </button>
+          </div>
+      </>
   );
 }
 
---
+++ src/images/alphabet/README.md
@@ -0,0 +1,5 @@
+# Icon Pack: Alphabet and numbers
+
+Attribution: [Alphabet icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/alphabet)
+
+https://www.flaticon.com/packs/alphabet-and-numbers-20?k=1654612830235
